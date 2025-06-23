14111a2ffb5062e5530326cc113f087d8078fecb
Display-Ketersediaan-Laboran <donnipernanda90@gmail.com>
2025-06-23 20:09:58 +0700

    update by donni

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -15,7 +15,7 @@ $current = basename($_SERVER['PHP_SELF']);
   <ul class="nav-links">
     <?php if ($role === 'laboran'): ?>
       <li><a href="../laboran/dashboard_laboran.php" class="<?= $current == 'dashboard_laboran.php' ? 'active' : '' ?>"><i class="bi bi-house-door-fill icon"></i>Dashboard</a></li>
-       <li><a href="../Super_Admin/tambah-ruangan.php" class="<?= $current == 'tambah-ruangan.php' ? 'active' : '' ?>"><i class="bi bi-plus-circle"></i> Rooms</a></li>
+       <li><a href="../Super_Admin/tambah-ruangan.php" class="<?= $current == 'tambah-ruangan.php' ? 'active' : '' ?>"><i class="bi bi-plus-circle"></i>Add Rooms</a></li>
       <li><a href="../laboran/list-ruangan.php" class="<?= $current == 'list-ruangan.php' ? 'active' : '' ?>"><i class="bi bi-building"></i>Rooms List</a></li>
       <li><a href="../laboran/add-matkul.php" class="<?= $current == 'add-matkul.php' ? 'active' : '' ?>"><i class="bi bi-plus-circle"></i>Add Courses</a></li>
        <li><a href="../laboran/list_matkul.php" class="<?= $current == 'list_matkul.php' ? 'active' : '' ?>"><i class="bi bi-building"></i>Courses List</a></li>
@@ -27,13 +27,13 @@ $current = basename($_SERVER['PHP_SELF']);
             class="bi bi-person-check-fill icon"></i>Status Dosen</a></li>
 
     <?php elseif ($role === 'superadmin'): ?>
-      <li><a href="../Super_Admin/dashboard.php" class="<?= $current == 'dashboard.php' ? 'active' : '' ?>">Dashboard</a></li>
-      <li><a href="../Super_Admin/tambah-ruangan.php" class="<?= $current == 'tambah-ruangan.php' ? 'active' : '' ?>">Add Rooms</a></li>
-      <li><a href="../Super_Admin/list-ruangan.php" class="<?= $current == 'list-ruangan.php' ? 'active' : '' ?>">Rooms List</a></li>
-      <li><a href="../Super_Admin/addmatkul.php" class="<?= $current == 'addmatkul.php' ? 'active' : '' ?>">Add Courses</a></li>
-      <li><a href="../Super_Admin/list_matkul.php" class="<?= $current == 'list_matkul.php' ? 'active' : '' ?>">Course List</a></li>
-      <li><a href="../Super_Admin/users_list.php" class="<?= $current == 'users_list.php' ? 'active' : '' ?>">Users List</a></li>
-      <li><a href="../Super_Admin/loan_data.php" class="<?= $current == 'loan_data.php' ? 'active' : '' ?>">Loan Data</a></li>
+      <li><a href="../Super_Admin/dashboard.php" class="<?= $current == 'dashboard.php' ? 'active' : '' ?>"><i class="bi bi-house-door-fill icon"></i>Dashboard</a></li>
+      <li><a href="../Super_Admin/tambah-ruangan.php" class="<?= $current == 'tambah-ruangan.php' ? 'active' : '' ?>"><i class="bi bi-plus-circle"></i>Add Rooms</a></li>
+      <li><a href="../Super_Admin/list-ruangan.php" class="<?= $current == 'list-ruangan.php' ? 'active' : '' ?>"><i class="bi bi-building"></i>Rooms List</a></li>
+      <li><a href="../Super_Admin/addmatkul.php" class="<?= $current == 'addmatkul.php' ? 'active' : '' ?>"><i class="bi bi-plus-circle"></i>Add Courses</a></li>
+      <li><a href="../Super_Admin/list_matkul.php" class="<?= $current == 'list_matkul.php' ? 'active' : '' ?>"><i class="bi bi-journal-text"></i>Courses List</a></li>
+      <li><a href="../Super_Admin/users_list.php" class="<?= $current == 'users_list.php' ? 'active' : '' ?>"><i class="bi bi-people-fill"></i>Users List</a></li>
+      <li><a href="../Super_Admin/loan_data.php" class="<?= $current == 'loan_data.php' ? 'active' : '' ?>"><i class="bi bi-calendar-check"></i>Loan Data</a></li>
     <?php else: ?>
       <li><span>Role tidak dikenali.</span></li>
     <?php endif; ?>
